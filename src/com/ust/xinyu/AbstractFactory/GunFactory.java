36333694c464862eebd9be1860e7a1d733a7ba13
ZHU Xinyu <xzhuah@connect.ust.hk>
2016-10-14 22:30:18 +0800

    Add some exercises

--- src/com/ust/xinyu/AbstractFactory/GunFactory.java
+++ src/com/ust/xinyu/AbstractFactory/GunFactory.java
@@ -5,4 +5,7 @@ package com.ust.xinyu.AbstractFactory;
  */
 public interface GunFactory {
     public GunPrototype createGun(int bulletNum);
+
+    //The following method is for your exercise, implement this method in right place so that the gun user can "buy" new right bullets from the right factory after they run out the ammo
+    //public BulletPrototype[] createBullet(int bulletNumber);
 }
